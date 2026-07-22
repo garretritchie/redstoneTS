dd2604812a28e59a9f70e88c3e8aff7f27b161d9
garretritchie <garretritchie@gmail.com>
2026-07-22 18:08:56 -0400

    Fix admin Supabase credentials and login

--- public/admin/redstone-admin.js
+++ public/admin/redstone-admin.js
@@ -1,6 +1,6 @@
 (function () {
-  var SUPABASE_URL = "https://0ec90b57d6e95fcbda19832f.supabase.co";
-  var SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJib2x0IiwicmVmIjoiMGVjOTBiNTdkNmU5NWZjYmRhMTk4MzJmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg4ODE1NzQsImV4cCI6MTc1ODg4MTU3NH0.9I8-U0x86Ak8t2DGaIk0HfvTSLsAyzdnz-Nw00mMkKw";
+  var SUPABASE_URL = "https://hnfdrolietieeqotxgqq.supabase.co";
+  var SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhuZmRyb2xpZXRpZWVxb3R4Z3FxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ3NDA3NTcsImV4cCI6MjEwMDMxNjc1N30.Avdto-_gKj5qInBXGp0yGY-JKShYOBEFuemwvjLbWtY";
 
   var SESSION_KEY = "redstone_cms_session";
   var source = window.REDSTONE_CMS_DATA || { site: {}, team: {}, insights: [] };
